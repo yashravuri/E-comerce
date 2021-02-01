b1fd8879a2987d758e6d6ca297a7a36640325ccb
yashravuri <78066066+yashravuri@users.noreply.github.com>
2021-02-01 15:46:20 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-fetch("../data/data.json").then(response=>{
+fetch("https://yashravuri.github.io/E-comerce/data/data.json").then(response=>{
     return response.json()
     }).then(data=>{
     displayData(data)
